b7685b6466bd6882c16885be5202442c626fd2de
Lange <40212673@live.napier.ac.uk>
2019-04-04 11:50:49 +0100

    added Tilemap

---
+++ res/tiled/Castle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Castle" tilewidth="32" tileheight="32" tilecount="672" columns="16">
+ <image source="../Desktop/Hazard-Dash/res/Sprites/castle_tileset_part1.png" width="512" height="1344"/>
+</tileset>
