77a162f413c6c95e6bcb11b97200733716c778fc
jimen7 <40204497@live.napier.ac.uk>
2019-04-23 22:00:26 +0100

    Fixed player offset and tile position, need to add Xbox controller. Also fixed trap- selection text when pressed

--- game_project/components/cmp_trap.cpp
+++ game_project/components/cmp_trap.cpp
@@ -50,40 +50,29 @@ void TrapComponent::update(double dt)
 	// convert it to world coordinate, because we scale in the render from 1080p to the target resolution
 	const sf::Vector2f worldPos = Engine::GetWindow().mapPixelToCoords(pixelPos);
 	const auto dir = Vector2f(worldPos) - _parent->getPosition();//Gets mouse potition in relation to tile's
-
-	//Vector2f mousePos = Vector2f(sf::Mouse::getPosition(Engine::GetWindow()));
-	//Vector2f mousePos = Vector2f(sf::Mouse::getPosition());
-	//tilePos = _parent->getPosition();
+	const auto l = sf::length(dir);
 
 	//cout << "Mouse Position:(" << mousePos << ")" << endl;
 	//cout << "Trap Position:(" << _parent->getPosition() << ")" << endl;
 
-
-	//const auto dir = mousePos - _parent->getPosition();//Gets mouse potition in relation to tile's
-//	const auto dir = mousePos - _parent->getPosition();//Gets mouse potition in relation to tile's
-	const auto l = sf::length(dir);
-
-	//auto s = _parent->get_components<ShapeComponent>();
-
-	auto c = ls::getTileAt(_parent->getPosition());
-		if (l <40.0) {
-
-			cout << "Trap Position:(" << _parent->getPosition() << ")" << endl;
+		//if (ls::getTileAt(worldPos)==ls::TRAP && ls::getTileAt(_parent->getPosition()) == ls::TRAP) {		//Tried to see if it was more efficient, it wasn't
+	if (l<25.0) {
+			//cout << "Trap Position:(" << _parent->getPosition() << ")" << endl;
 			_trap_colour = _selected_trap_colour;	//Set the print colour tpo be highlighted
 		
 
 			
 
-		}
-		else {
+	}
+	else {
 			_trap_colour = _original_trap_colour;	//Set the print colout to be the original
-		}
+	}
 
 	if (_timer >= 0) {
 		_timer -= dt;
 	}
 	else{
-		auto collidingObjects = _parent->GetCompatibleComponent<PhysicsComponent>()[0]->getTouching();
+		auto collidingObjects = _parent->GetCompatibleComponent<PhysicsComponent>()[0]->getTouching();//Gets any colliding objects with the trap, improved from checking against every hero
 
 		for (auto k : collidingObjects) {
 			Entity* e1 = (Entity*)k->GetFixtureA()->GetUserData();
@@ -113,7 +102,7 @@ void TrapComponent::update(double dt)
 void TrapComponent::render() {
 	_rs.setFillColor(_trap_colour);
 	_rs.setSize({ tileSizeTEMP ,tileSizeTEMP });
-	_rs.setPosition(_parent->getPosition() + Vector2f(-tileSizeTEMP/2.0f, -tileSizeTEMP / 2.0f));
+	_rs.setPosition(_parent->getPosition() + Vector2f(-tileSizeTEMP/ DIVIDER, -tileSizeTEMP / DIVIDER));
 	Renderer::queue(&_rs);
 }
 
